cd87478f94eec3ef327a46c5f6b594507f2b900b
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-06-22 23:11:02 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -41,3 +41,27 @@ alter table animals drop column species;
 alter table animals add column species_id integer references species(id);
 --Add column owner_id which is a foreign key referencing the owners table
 alter table animals add column owner_id integer references owners(id);
+
+--day4
+--Create a table named vets
+create table vets (
+    id serial PRIMARY KEY,
+    name varchar(50),
+    age integer,
+    date_of_graduation date
+);
+
+--Create a "join table" called specializations
+create table specializations (
+    species_id integer references species(id),
+    vets_id integer references vets(id),
+    primary key (species_id, vets_id)   
+);
+
+--Create a "join table" called visits
+create table visits (
+    animals_id integer references animals(id),
+    vets_id integer references vets(id),
+    date_of_visit date,
+    primary key (animals_id, vets_id, date_of_visit)
+);    
